fcd5510ea4c87fed4549f4d9a8fc87eb8921d03e
priya 123 <priyakatare1707@gmail.com>
2025-11-19 17:31:23 +0530

    new

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,7 @@
-n=int(input ('enter row'))
-for i in range(1,n+1):
-    print(' '*(n-i)+'* '*i)
+n = int(input("Enter number of rows: "))
+# # Decreasing stars, right-aligned
+# for i in range(n, 0, -1):
+#     print('  ' * (n - i) + '* ' * i)
+# # Increasing stars, right-aligned
+# for i in range(2, n + 1):
+#     print('  ' * (n - i) + '* ' * i)
