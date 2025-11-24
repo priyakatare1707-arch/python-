5568637d3cac74b9ec61c63aa4d9f5659b4ea258
priya 123 <priyakatare1707@gmail.com>
2025-11-25 00:30:32 +0530

    new

--- function.py
+++ function.py
@@ -48,3 +48,99 @@
 # add(p,q)
 # print(add(p,q))
 # #return value depend on function
+#---------------------------------------------------key ward argument----------------------------------------------------------------
+# def fan_name(x,y,z):
+#     print(z)
+#     print(y)
+#     print(x)
+# p=int(input('enter a num'))
+# q=int(input('enter a num'))
+# r=int(input('enter a num'))
+# fan_name(z=p,y=q,x=r)
+#-------------------------------------------------default key ward argument---------------------------------
+# def fan_name(x=0,y=0,z=0):
+#     print(z)
+#     print(y)
+#     print(x)
+# p=int(input('enter a num'))
+# q=int(input('enter a num'))
+# r=int(input('enter a num'))
+# fan_name()
+# fan_name(z=p)
+# fan_name(z=p,x=r)
+#---------------------------------------------------------vaeiable length key ward argument---------------------------------------------
+# def fan_name(**kwargs):
+#     print(kwargs)
+#     print(type(kwargs))
+   
+# fan_name(x=10,y=20,z=30,p=2,q=5)
+
+# def fan_name(**kwargs):#--->paking
+#     print(kwargs)
+#     print(type(kwargs))
+   
+# fan_name(**eval(input("enter any dic:")))#--->unpaking
+#     print(kwargs)
+
+# def fan_name(**kwargs):
+#     for i in kwargs.keys():
+#        print(i)
+#     for i in kwargs.values():
+#         print(i)
+#     for i,j in kwargs.items():
+#         print('key=',i,'value=',j)
+# fan_name(**eval(input("enter any dic:")))
+
+# key= x value= 10
+# key= y value= 30
+
+
+# def fan_name(x,y=0,*z,p,**q):
+#     print(x)
+#     print(y)
+#     print(z)
+#     print(p)
+#     print(q)
+# fan_name(10,20,30,40,50,p=5,r=2,s=1,t=3)
+
+# def fan_name(x,y=0,*z,p,**q):
+#     print(x)
+#     print(y)
+#     print(z)
+#     print(p)
+#     print(q)
+# fan_name(10,p=5,20,30,40,50,r=2,s=1,t=3)
+
+# def natural_num(n):
+#     sum=0
+#     for i in range(1,n+1):
+#         sum+=i
+#     print(sum)   
+# n=int(input('enter a num'))
+# natural_num(n)
+
+# def natural_num(n):
+    
+#     for i in range(1,n+1):
+#         if i%2==0:
+#            print(i)   
+# n=int(input('enter a num'))
+# natural_num(n)
+
+def natural_num(n):
+    count=0
+    for i in range(1,n+1):
+        if n % i==0:
+            count+=1
+    if count > 2:
+        return ' Not prime'
+    else:
+        return 'prime'
+           
+n=int(input('enter a num'))
+res=natural_num(n)
+print(res)
+
+
+
+
